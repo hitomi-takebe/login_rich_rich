6f771903da5ee4cc9290625a7ce3c890c5e3bfee
Hitomi Takebe <takebe.hitomi@gmail.com>
2024-12-22 00:37:32 +0900

    Add files via upload

---
+++ php/welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <style>
+    .link {
+    max-width: 400px;
+    }
+    </style>
+</head>
+<body>
+    <p class="card__title">ログイン成功です！</p>
+    <p class="link"><a href="../index.php">戻る</a></p>
+</body>
+</html>
